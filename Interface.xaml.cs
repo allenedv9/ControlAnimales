2776ea1df2d301b1b2d909a3a706aa3d1108eeb4
allenedv9 <allendeedv@gmail.com>
2021-02-05 13:00:34 +0100

    q

--- Interface.xaml.cs
+++ Interface.xaml.cs
@@ -42,19 +42,27 @@ namespace ControlAnimales
 
 
 
-        private void foco(object sender, DependencyPropertyChangedEventArgs e){
+        private void foco(object sender, DependencyPropertyChangedEventArgs e)
+        {
 
-            switch
+
             if (txt_nombre.Focus() || txt_fecha.Focus() || txt_edad.Focus() || txt_color.Focus() ||
                 txt_cartilla.Focus() || txt_chip.Focus() || txt_fecha_adopcion.Focus() || txt_lugar_adopcion.Focus())
             {
 
-               
+                //comentario
+                txt_nombre.Text = "";
+                txt_fecha.Text = "";
+                txt_edad.Text = "";
+                txt_color.Text = "";
+                txt_cartilla.Text = "";
+                txt_chip.Text = "";
+                txt_fecha_adopcion.Text = "";
                 txt_lugar_adopcion.Text = "";
 
 
             }
         }
-    
-    }                                                  
-                                      
+
+    }
+}                         
