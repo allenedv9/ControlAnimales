46a1bb2f894bddceff751acdb0c366269a5b6940
Aida <aidadu@msn.com>
2021-02-06 23:36:22 +0100

    a

--- Interface.xaml.cs
+++ Interface.xaml.cs
@@ -17,12 +17,12 @@ namespace ControlAnimales
     /// </summary>
     public partial class Interface : Window
     {
-       
+
         public Interface()
         {
             InitializeComponent();
 
-         
+
         }
 
         private void añadir_mascota_Click(object sender, RoutedEventArgs e)
@@ -42,37 +42,43 @@ namespace ControlAnimales
 
         }
 
-
-
+        //Limpia textBox para recojer dato
         private void foco(object sender, RoutedEventArgs e)
         {
-       
-            if (txt_nombre.IsFocused) {
+
+            if (txt_nombre.IsFocused)
+            {
                 txt_nombre.Text = "";
             }
-            if (txt_fecha.IsFocused){
+            if (txt_fecha.IsFocused)
+            {
                 txt_fecha.Text = "";
             }
-           
-            if (txt_color.IsFocused) {
+
+            if (txt_color.IsFocused)
+            {
                 txt_color.Text = "";
             }
-            if (txt_cartilla.IsFocused){
+            if (txt_cartilla.IsFocused)
+            {
                 txt_cartilla.Text = "";
             }
-            if (txt_chip.IsFocused) {
+            if (txt_chip.IsFocused)
+            {
                 txt_chip.Text = "";
             }
-            if (txt_fecha_adopcion.IsFocused){
+            if (txt_fecha_adopcion.IsFocused)
+            {
                 txt_fecha_adopcion.Text = "";
             }
-            if (txt_lugar_adopcion.IsFocused){
+            if (txt_lugar_adopcion.IsFocused)
+            {
                 txt_lugar_adopcion.Text = "";
             }
-           
+
         }
 
-
+        //Si el textBox esta vacio vuelve a colocar el nombre del campo a rellenar
         private void perder_foco(object sender, RoutedEventArgs e)
         {
             if (txt_nombre.Text == "")
@@ -101,7 +107,7 @@ namespace ControlAnimales
             }
             if (txt_fecha_adopcion.Text == "")
             {
-                txt_fecha_adopcion.Text = "";
+                txt_fecha_adopcion.Text = "Fecha Adopcion";
             }
             if (txt_lugar_adopcion.Text == "")
             {
@@ -115,19 +121,20 @@ namespace ControlAnimales
             //recojo mes y año actual
             String anoActual = DateTime.Today.ToString("yyyy");
             fecha.DisplayDate.Year.ToString();
+            fecha.DisplayDate.Month.ToString();
+            String mesActual = DateTime.Today.ToString("MM");
+
+            //recojo mes y año nacimiento
+            String mesNacimiento = fecha.DisplayDate.Month.ToString();
             String anoNacimiento = fecha.DisplayDate.Year.ToString();
             txt_fecha.Text = anoNacimiento;
-           
-          
-            //recojo mes y año nacimiento
-            String mesActual = DateTime.Today.ToString("MM");
-            fecha.DisplayDate.Month.ToString();
-            String mesNacimiento = fecha.DisplayDate.Month.ToString();
-            
+
+       
+
             //calcula tiempo transcurrido
             int anos = Int32.Parse(anoActual) - Int32.Parse(anoNacimiento);
             int meses = Int32.Parse(mesActual) - Int32.Parse(mesNacimiento);
-          
+
 
             //Muestra edad en años y meses y si es menor de un año solo meses
             if (meses < 0)
@@ -135,15 +142,18 @@ namespace ControlAnimales
                 anos = anos - 1;
                 meses = 12 + meses;
 
-                if (anos == 0){
+                if (anos == 0)
+                {
                     txt_edad.Text = meses.ToString() + " meses";
-                }else  if (anos >= 2)
+                }
+                else if (anos >= 2)
                 {
                     txt_edad.Text = anos.ToString() + " años y " + meses.ToString() + " meses.";
                 }
                 else
                     txt_edad.Text = anos.ToString() + " año y " + meses.ToString() + " meses.";
-            } else
+            }
+            else
             {
                 if (anos >= 2)
                 {
@@ -154,5 +164,54 @@ namespace ControlAnimales
             }
 
         }
+
+        //Elige la opcion de macho
+        private void elige_macho(object sender, RoutedEventArgs e)
+        {
+            if (macho.IsChecked == true)
+            {
+                txt_sexo.Text = "macho";
+            }
+        }
+
+        //Elige la opcion de hembra
+        private void elige_hembra(object sender, RoutedEventArgs e)
+        {
+            if (hembra.IsChecked == true)
+            {
+                if (hembra.IsChecked == true)
+                {
+                    txt_sexo.Text = "hembra";
+                }
+            }
+        }
+
+        //Muestra los campos (ocultos)  si es adoptado
+        private void ver_txt_adopcion(object sender, RoutedEventArgs e)
+        {
+            if (adoptado.IsChecked==true) {
+                txt_lugar_adopcion.Visibility = Visibility.Visible;
+                txt_fecha_adopcion.Visibility = Visibility.Visible;
+                dp_fecha_adopcion.Visibility = Visibility.Visible;
+            }
+            else{
+                txt_lugar_adopcion.Visibility = Visibility.Hidden;
+                txt_fecha_adopcion.Visibility = Visibility.Hidden;
+                dp_fecha_adopcion.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void fecha_adopcion(object sender, RoutedEventArgs e)
+        {
+            String fecha_adopcion = dp_fecha_adopcion.DisplayDate.ToString();
+              
+            //falta dar formato para que recoja bien dia mes y año
+            //solo recoje mes y año
+            txt_fecha_adopcion.Text=fecha_adopcion;
+           
+
+
+        }
     }
-}                         
+}
+                        
