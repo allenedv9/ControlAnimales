67567cb21efee0a441dfd3c1609ac1f46b0792e6
Aida <aidadu@msn.com>
2021-02-25 17:51:17 +0100

    a

--- Interface.xaml.cs
+++ Interface.xaml.cs
@@ -36,13 +36,15 @@ namespace ControlAnimales
             InitializeComponent();
 
         //  conexion AIDA 
-        //  string conexion = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Aida\\Desktop\\PERRUNO\\ControlAnimales\\Mascotas .mdf;Integrated Security=True;Connect Timeout=30";
+        string conexion = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Aida\\Desktop\\PERRUNO\\ControlAnimales\\Mascotas .mdf;Integrated Security=True;Connect Timeout=30";
         //  conexion ALLENDE
-            conexion = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Allende\\source\\repos\\ControlAnimales\\Mascotas.mdf;Integrated Security=True;Connect Timeout=30";
+          //    conexion = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Allende\\source\\repos\\ControlAnimales\\Mascotas.mdf;Integrated Security=True;Connect Timeout=30";
 
             con = new SqlConnection(conexion);
             cargarEspecies();
             cargaMascotas();
+            cargaMascotasVet();
+            cargarVeterinario();
         }
 
 
@@ -121,8 +123,69 @@ namespace ControlAnimales
         /*****************************************************************************************************************************/
         //MODIFICAR LOS DATOS DE LA PANTALLA VETERINARIO//
         /*****************************************************************************************************************************/
-       
-        
+
+        //carga combo de mascotas en vista veterinario
+        private void cargaMascotasVet()
+        {
+            try
+            {
+                //abrimos la conexion
+                con.Open();
+            }
+            catch (Exception ee) { MessageBox.Show(ee.Message); }
+            string query = "SELECT * FROM mascota";
+            SqlDataAdapter miAdaptadorSql = new SqlDataAdapter(query, con);
+
+            using (miAdaptadorSql)
+            {
+                DataTable mascota = new DataTable();
+                miAdaptadorSql.Fill(mascota);
+                nombreMascotasVet.DisplayMemberPath = "nombre";
+                nombreMascotasVet.SelectedValuePath = "id_mascota";
+                nombreMascotasVet.ItemsSource = mascota.DefaultView;
+            }
+            con.Close();
+        }
+
+        //carga combo de veterinario en vista veterinario
+        private void cargarVeterinario()
+        {
+            try
+            {
+                //abrimos la conexion
+                con.Open();
+            }
+            catch (Exception ee) { MessageBox.Show(ee.Message); }
+            string query = "SELECT * FROM veterinario";
+            SqlDataAdapter miAdaptadorSql = new SqlDataAdapter(query, con);
+
+            using (miAdaptadorSql)
+            {
+                DataTable veterinario = new DataTable();
+                miAdaptadorSql.Fill(veterinario);
+                carga_vet.DisplayMemberPath = "clinica";
+                carga_vet.SelectedValuePath = "id_veterinario";
+                carga_vet.ItemsSource = veterinario.DefaultView;
+            }
+            con.Close();
+        }
+        private void cargar_masc_veterinario(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox cb = (ComboBox)sender;
+
+            DataRowView drv = (DataRowView)cb.SelectedItem;
+            string cadena3 = drv.Row[1].ToString();
+            mascota_visita.Text = cadena3;
+        }
+
+        private void Clinica_selescciona(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox cb = (ComboBox)sender;
+
+            DataRowView drv = (DataRowView)cb.SelectedItem;
+            string cadena3 = drv.Row[1].ToString();
+            txt_clinica_vet.Text = cadena3;
+        }
         //INSERT VETERINARIO
         private void Guardar_Veterinaria(object sender, RoutedEventArgs e)
         {
@@ -424,7 +487,7 @@ namespace ControlAnimales
             try
             {
                 //abrimos la conexion
-                MessageBox.Show("");
+        
                 con.Open();
             }
             catch (Exception ee)
